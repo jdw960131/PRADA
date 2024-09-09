667934d86c1a2c0333ccec4f53a1a7259b4f64cd
kanghyejin1105 <rkddpwls666@gmail.com>
2024-09-09 17:38:41 +0900

    월요일업로드

--- js/about.js
+++ js/about.js
@@ -1,21 +1,40 @@
 $(document).ready(function(){
+  let i = 0;
+  let count = $(".main_img>li").length;
 
-  $(".tab_bt>li").click(function(){
-    let list = $(this).index();
-    $(".tab_conts>li").stop().fadeOut();
-    $(".tab_conts>li").eq(list).stop().fadeIn();
-    $(".tab_bt>li").removeClass("on")
-    $(this).addClass("on")
+  // let auto = setInterval(function(){
+  //   i = $(this).index();
+  //   dongwoo();
+  // },1000);
+
+
+  let autoSlide = setInterval(function(){
+    if(i == count-1){
+      i=0;
+    }else{
+      i++;
+    }
+      dongwoo()
+  },5000);
+
+
+  $(".nav>li").click(function(){
+    i = $(this).index();
+      dongwoo();
   });
 
-  let auto = setInterval(function(){
-    asd();
-  },7000);
+
+function dongwoo(){
+  $(".nav>li").removeClass("in");
+  $(".nav>li").eq(i).addClass("in");
+  $(".main_img>li").stop().fadeOut(100);
+  $(".main_img>li").eq(i).stop().fadeIn(100);
+}
+
 
   $("#header .logo .menu").click(function(){
     $(".ham").stop().animate({"left":"0px"});
   });
-
   $("#header .ham .ham_gnb_home .close_bt").click(function(){
     $(".ham").stop().animate({"left":"-100%"});
   });
@@ -29,28 +48,30 @@ $(document).ready(function(){
     }
   });
 
-$(".ham_gnb_home>li:first-child").click(function(){
-  if($(this).hasClass("run")){
-    $(".ham_gnb_home>li:first-child").removeClass("run");
-  }else{
-    $(".ham_gnb_home>li:first-child").removeClass("run");
-    $(this).addClass("run");
-  }
-});
+  $(".ham_gnb_home>li:first-child").click(function(){
+    if($(this).hasClass("run")){
+      $(".ham_gnb_home>li:first-child").removeClass("run");
+    }else{
+      $(".ham_gnb_home>li:first-child").removeClass("run");
+      $(this).addClass("run");
+    }
+  });
 
-$(".change_group>li").click(function(){
-  if($(this).hasClass("acco")){
-    $(".change_group>li").removeClass("acco");
-  }else{
-    $(".change_group>li").removeClass("acco");
-    $(this).addClass("acco");
-  }
-});
+  $(".change_group>li").click(function(){
+    if($(this).hasClass("acco")){
+      $(".change_group>li").removeClass("acco");
+    }else{
+      $(".change_group>li").removeClass("acco");
+      $(this).addClass("acco");
+    }
+  });
 
-  function asd(){
-    $(".main_img").stop().animate({"margin-top":"-200vh"},1000,function(){
-        $(".main_img>li:first-child").appendTo(".main_img");
-        $(".main_img").css({"margin-top":"-100vh"});
-    });
-  }
+  $(".tab_bt>li").click(function(){
+    let list = $(this).index();
+    $(".tab_conts>li").stop().fadeOut();
+    $(".tab_conts>li").eq(list).stop().fadeIn();
+    $(".tab_bt>li").removeClass("on")
+    $(this).addClass("on")
+  });
+
 });
